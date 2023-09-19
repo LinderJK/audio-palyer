1d9facee4deedd47e7c19ea3f0465459123715b4
ilya slepchenkov <slepchenkovia@gmail.com>
2023-09-19 22:04:51 +0300

    feat: add change track functions

--- audio-player/js/script.js
+++ audio-player/js/script.js
@@ -1,55 +1,89 @@
-let track = {
-   0: './assets/music/Libercio-feeling_alright.mp3',
-   1: './assets/music/Libercio-feeling_alright.mp3',
+let trackList = [
+  "Arkady_Antsyrev-Ne_vspominaem",
+  "Ishome-Ken_Tavr",
+  "Pobeg-Mosty"
+];
+
+
+const autorSong = document.querySelector('#songAutor');
+const nameSong = document.querySelector('#songName');
+const coverSong = document.querySelector('#songCover');
+const audioPlayer = document.querySelector('.player');
+
+
+const audio = new Audio();
+let isPlay = false;
+let currentTrack = trackList[0];
+let trackListLength = trackList.length - 1;
+updatePlayer(currentTrack);
+
+
+function updatePlayer(song) {
+  const info = song.split('-');
+  autorSong.innerHTML = info[0].replace(/_/g, ' ');
+  nameSong.innerHTML = info[1].replace(/_/g, ' ');
+  audio.src = `./assets/music/${song}.mp3`;
+  coverSong.src = `./assets/image/${song}.png`;
+}
+
+const buttonNextSong = document.querySelector('#buttonNext');
+buttonNextSong.addEventListener('click', nextSong);
+
+function nextSong() {
+  let currentTrackIndex = trackList.indexOf(currentTrack);
+  console.log('currentTrackIndex', currentTrackIndex);
+  if (currentTrackIndex < trackListLength) {
+    currentTrack = trackList[currentTrackIndex + 1];
+    updatePlayer(currentTrack);
+    playSong();
+    console.log('currentTrackIndex', currentTrackIndex);
+  }
+  if (currentTrackIndex === trackListLength) {
+    currentTrack = trackList[0];
+    updatePlayer(currentTrack);
+    playSong();
+  }
+}
+
+const buttonPrevSong = document.querySelector('#buttonPrev');
+buttonPrevSong.addEventListener('click', prevSong);
+
+function prevSong() {
+  let currentTrackIndex = trackList.indexOf(currentTrack);
+  console.log('currentTrackIndex', currentTrackIndex);
+  if (currentTrackIndex !== 0) {
+    currentTrack = trackList[currentTrackIndex - 1];
+    updatePlayer(currentTrack);
+    playSong();
+    console.log('currentTrackIndex', currentTrackIndex);
+  }
+  if (currentTrackIndex === 0) {
+    currentTrack = trackList[trackListLength];
+    updatePlayer(currentTrack);
+    playSong();
+  }
 }
 
 
 
 
+const buttonPlay = document.querySelector('#buttonPause');
+buttonPlay.addEventListener('click', () => {
+  if (!isPlay) {
+    playSong();
+  } else {
+    pauseSong();
+  }
+})
 
-
-
-
-let defaultSongIndex = 0;
-updatePlayer()
-function updatePlayer (song) {
-
-
+function playSong() {
+  audio.volume = 0.5;
+  isPlay = true;
+  audio.play();
 }
 
+function pauseSong() {
+  isPlay = false;
+  audio.pause();
 
-const contentDiv = document.querySelector('.player');
-document.body.onload =()=> addPlayer(track[0]);
-
-
-function addPlayer (trackUrl) {
-    const player = new Audio();
-    contentDiv.appendChild(player);
-    player.setAttribute('src', trackUrl);
-    playerButton();
 }
-
-
-
-
-
-
-// function playerButton () {
-//     let buttonNext = document.createElement('button');
-//     buttonNext.classList.add('button');
-//     buttonNext.classList.add('button-next');
-//     contentDiv.appendChild(buttonNext);
-
-//     let buttonPrev = document.createElement('button');
-//     buttonPrev.classList.add('button');
-//     buttonPrev.classList.add('button-prev');
-//     contentDiv.appendChild(buttonPrev);
-
-//     let buttonPause = document.createElement('button');
-//     buttonPause.classList.add('button');
-//     buttonPause.classList.add('button-pause');
-//     contentDiv.appendChild(buttonPause);
-// }
-
-
-
